0ed5ef2f48799b7a515abc4cff5f01975ae55fdd
Vibhanshu <vibhanshu86@gmail.com>
2018-01-23 21:51:54 +0530

    enable media upload

--- main/urls.py
+++ main/urls.py
@@ -21,4 +21,5 @@ from django.contrib import admin
 urlpatterns = [
                   url(r'^admin/', admin.site.urls),
                   url(r'^', include('appRegistration.urls')),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
